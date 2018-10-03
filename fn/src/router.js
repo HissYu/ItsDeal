e15daf3afd56375dbb6f287cafb180f077cd849a
hissyu <hissyu@hotmail.com>
2018-10-03 22:49:04 +0800

    v0.0.7

--- fn/src/router.js
+++ fn/src/router.js
@@ -2,12 +2,14 @@ import homepage from './components/index/homepage';
 import lookaround from './components/index/lookaround';
 import login from './components/index/login';
 import register from './components/index/register';
+import profile from './components/index/profile';
 
 const routes = [
     {path:'/', component: homepage},
     {path:'/walkaround',component: lookaround},
     {path:'/login',component:login},
     {path:'/register',component:register},
+    {path:'/profile',component:profile},
 ]
 
 export default routes
