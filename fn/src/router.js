b9db003e93efbcc560e2a1b29f4e786e6ff4ee23
hissyu <hissyu@hotmail.com>
2018-10-05 14:27:32 +0800

    v0.0.9 ps previos commit should be v0.0.8.1

--- fn/src/router.js
+++ fn/src/router.js
@@ -4,6 +4,7 @@ import login from './components/index/login';
 import register from './components/index/register';
 import profile from './components/index/profile';
 import item from './components/index/item';
+import putaway from './components/index/putaway';
 
 const routes = [
     {path:'/', component: homepage},
@@ -12,6 +13,7 @@ const routes = [
     {path:'/register',component:register},
     {path:'/profile',component:profile},
     {path:'/item',component:item},
+    {path:'/putaway',component:putaway},
     
 ]
 
