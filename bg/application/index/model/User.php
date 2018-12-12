ad3b3684450bc56e0a87010d7a7bff4134b58401
hissyu <hissyu@hotmail.com>
2018-12-12 10:19:15 +0800

    add some useless

--- bg/application/index/model/User.php
+++ bg/application/index/model/User.php
@@ -3,45 +3,12 @@ namespace app\user\model;
 
 use think\Model;
 use think\common;
-class User extends Model
+
+/**
+ * all base users' functions
+ */
+trait BaseUser
 {
-    /**
-     * this return given user's basic infomation
-     * @author ORIGIN
-     * @param int user's ID
-     * @return array user's basic infomation
-     */
-    public function getUserInfo($userId)
-    {
-        $map['user_id']=$userId;
-        $condition=['dl_seller','dl_seller.seller_id=user_id'];
-        $field=[
-            'user_id',
-            'user_nickname',
-            'user_location',
-            'seller_history',
-            'user_is_seller',
-            'user_is_verified',
-            '_hide_detail'
-        ];
-        $data=$this->table('dl_user')->join($condition)->field($field)->where($map)->select();
-        return $data;
-    }
-    /**
-     * this returns given user's detail,second parameter decides query's result
-     * @author ORIGIN
-     * @param int user's ID
-     * @param boolean if you want HistoryOnly Mode
-     * @return array query result
-     */
-    public function getUserDetail($userId,$onlyHistory)
-    {
-        $map['user_id']=$userId;
-        if($onlyHistory) $field=['user_history'];
-        else $field=['user_realname','user_stu_id','user_history'];
-        $data=$this->table('dl_user_detail')->field($field)->where($map)->select();
-        return $data;
-    }
     /**
      * this presents check for user logging in
      * @author ORIGIN
@@ -94,6 +61,49 @@ class User extends Model
         $data=$this->table('dl_user')->where('user_nickname',$userNickName)->count();
         return $data;
     }
+}
+
+class User extends Model implements IUser
+{
+    use BaseUser;
+    /**
+     * this return given user's basic infomation
+     * @author ORIGIN
+     * @param int user's ID
+     * @return array user's basic infomation
+     */
+    public function getUserInfo($userId)
+    {
+        $map['user_id']=$userId;
+        $condition=['dl_seller','dl_seller.seller_id=user_id'];
+        $field=[
+            'user_id',
+            'user_nickname',
+            'user_location',
+            'seller_history',
+            'user_is_seller',
+            'user_is_verified',
+            '_hide_detail'
+        ];
+        $data=$this->table('dl_user')->join($condition)->field($field)->where($map)->select();
+        return $data;
+    }
+    /**
+     * this returns given user's detail,second parameter decides query's result
+     * @author ORIGIN
+     * @param int user's ID
+     * @param boolean if you want HistoryOnly Mode
+     * @return array query result
+     */
+    public function getUserDetail($userId,$onlyHistory)
+    {
+        $map['user_id']=$userId;
+        if($onlyHistory) $field=['user_history'];
+        else $field=['user_realname','user_stu_id','user_history'];
+        $data=$this->table('dl_user_detail')->field($field)->where($map)->select();
+        return $data;
+    }
+    
     /**
      * when user apply to verify his account
      * @author ORIGIN
